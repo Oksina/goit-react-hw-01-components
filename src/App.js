69d5cf8e9d927f03579c91f5229def652926c03a
Oksana Yurchenkovych <oksanayurchenkovych@gmail.com>
2021-03-17 14:09:52 +0200

    123

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React from "react";
+import React from 'react';
 
 //імпорт компонентів
-import Profile from "./components/Profile/Profile";
-import Statistics from "./components/Statistics/Statistics";
-import FriendsList from "./components/FriendList/FriendsList";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
+import FriendsList from './components/FriendList/FriendsList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
 //імпорт баз даних
-import user from "../src/components/Profile/user.json";
-import statisticalData from "../src/components/Statistics/statistical-data.json";
-import friends from "../src/components/FriendList/friends.json";
-import transactions from "../src/components/TransactionHistory/Transaction.json";
+import user from '../src/components/Profile/user.json';
+import statisticalData from '../src/components/Statistics/statistical-data.json';
+import friends from '../src/components/FriendList/friends.json';
+import transactions from '../src/components/TransactionHistory/Transaction.json';
 
 function App() {
-  return (
-    <div>
-      <Profile
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={statisticalData} />
-      <Statistics stats={statisticalData} />
-      <FriendsList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </div>
-  );
+    return (
+        <div>
+            <Profile
+                name={user.name}
+                tag={user.tag}
+                location={user.location}
+                avatar={user.avatar}
+                stats={user.stats}
+            />
+            <Statistics title="Upload stats" stats={statisticalData} />
+            <Statistics stats={statisticalData} />
+            <FriendsList friends={friends} />
+            <TransactionHistory items={transactions} />
+        </div>
+    );
 }
 
 export default App;
