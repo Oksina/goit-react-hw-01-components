a23d1b0ea0104d58f56a9c8b994f96b417a5a64c
Oksana Yurchenkovych <oksanayurchenkovych@gmail.com>
2021-03-23 16:57:57 +0200

    1

--- src/App.js
+++ src/App.js
@@ -22,8 +22,7 @@ function App() {
                 avatar={user.avatar}
                 stats={user.stats}
             />
-            <Statistics title="Upload stats" stats={statisticalData} />
-            <Statistics stats={statisticalData} />
+            <Statistics title="Statistics" stats={statisticalData} />
             <FriendsList friends={friends} />
             <TransactionHistory items={transactions} />
         </>
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
 
 const Statistics = ({ title, stats }) => {
     //console.log(stats);
     return (
-        <section className="statistics">
-            <h2 className="title">{title}</h2>
-            {title && (
-                <ul className="stat-list">
-                    {stats.map(el => (
-                        <li key={el.id} className="item">
-                            <span className="label">{el.label}</span>
-                            <span className="percentage">{el.percentage}%</span>
+        <section className={s.statistics}>
+            {title && <h2 className={s.title}>Statistics</h2>}
+
+            <ul className={s.statList}>
+                {stats.map(el => {
+                    const { id, label, percentage } = el;
+                    return (
+                        <li className={s.item} key={id}>
+                            <span className={s.label}>{label} </span>
+                            <span className={s.percentage}>{percentage}%</span>
                         </li>
-                    ))}
-                </ul>
-            )}
+                    );
+                })}
+            </ul>
         </section>
     );
 };
@@ -23,13 +26,14 @@ const Statistics = ({ title, stats }) => {
 export default Statistics;
 
 Statistics.defaultProps = {
-    title: '',
+    title: 'string',
 };
 
 Statistics.propTypes = {
     title: PropTypes.string,
     stats: PropTypes.arrayOf(
         PropTypes.shape({
+            id: PropTypes.string.isRequired,
             label: PropTypes.string.isRequired,
             percentage: PropTypes.number.isRequired,
         }),
